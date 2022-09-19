149bcdb41c0b25ae2919a29ada8898785bdc9124
yuto <h.yyt9n@gmail.com>
2022-09-19 09:07:45 +0900

    Authentication Page作成、Next.jsで画面挙動確認済み

---
+++ pages/dashboard.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import axios from 'axios'
+import { LogoutIcon } from '@heroicons/react/solid'
+import { Layout } from "../components/Layout";
+
+const Dashboard: NextPage = () => {
+    const router = useRouter()
+    const logout =async () => {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
+        router.push('/')
+    }
+    return (
+        <Layout title="Task Board">
+            <LogoutIcon
+                className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
+                onClick={logout}
+            />
+        </Layout>
+    )
+}
+
+export default Dashboard
